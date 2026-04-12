a48450d954a6b8b5cde4fa528584d60ac86bf7d7
Feyi Akinola <feyi.akinola.3005@gmail.com>
2026-04-12 20:35:52 +0100

    Added missing prop in Button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,11 +1,12 @@
 type ButtonProps = {
   text: string;
+  className?: string;
 }
 
-const Button = ({ text }: ButtonProps ) => {
+const Button = ({ text, className }: ButtonProps ) => {
   return (
-    <div className="bg-white text-black/80 rounded-2xl py-4 px-16 flex-center cursor-pointer
-      hover:bg-white/70 transition-all duration-300">
+    <div className={`bg-white text-black/80 rounded-2xl py-4 px-16 flex-center cursor-pointer
+      hover:bg-white/70 transition-all duration-300 ${className}`}>
       <p className="text-lg font-bold">{text}</p>
     </div>
   );
